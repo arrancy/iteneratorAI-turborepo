6076d5269377116415f846e9bde41e456186f6fd
arrancy <ruturajchondekar@gmail.com>
2026-07-22 00:00:13 +0530

    added proper zod schema

--- apps/itenerator-ws-server/src/zodTypes/zodTypes.ts
+++ apps/itenerator-ws-server/src/zodTypes/zodTypes.ts
@@ -1,6 +1,8 @@
 import z from "zod";
 
+const cuidSchema = z.string().regex(/^c[a-z0-9]{24,}$/);
 const wsMessageType = z.object({
+  tripId: cuidSchema,
   type: z.enum(["message"]),
   content: z.string().min(1).max(10000),
 });
